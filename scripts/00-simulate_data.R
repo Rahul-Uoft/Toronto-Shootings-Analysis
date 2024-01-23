908a2d69e5838852861c44316ebaf22dab1cce76
Rahul Gopeesingh <rahulgopeesingh@Rahuls-MacBook-Air.local>
2024-01-23 02:16:36 -0500

    Simulated table working

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates a data set for the total number of shootings in Toronto for the years 2014-2019 in different geographic locations
+# Author: Rahul Gopeesingh
+# Date: 23 January 2024
+# Contact: rahul.gopeesingh@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: -
+
 
 
 #### Workspace setup ####
@@ -14,6 +14,22 @@ library(tidyverse)
 
 #### Simulate data ####
 # [...ADD CODE HERE...]
+
+set.seed(42)
+
+simulated_data_shootings <- 
+  tibble(
+    division = rep(c("D11","D12","D13","D14","D22","D23","D31","D32","D33","D41","D42","D43","D51","D52","D53","D54/55"), times = 6),
+    year = c(rep("2014", times = 16),
+             rep("2015", times = 16),
+             rep("2016", times = 16),
+             rep("2017", times = 16),
+             rep("2018", times = 16),
+             rep("2019", times = 16)),
+    number_of_shootings =
+      runif(96, 0, 70)
+  )
+simulated_data_shootings
 
 
 
