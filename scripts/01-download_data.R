2156f8d9c0564475a7f61d7a529575c526bb1caa
Rahul Gopeesingh <rahulgopeesingh@Rahuls-MacBook-Air.local>
2024-01-23 19:08:09 -0500

    Added script for importing raw data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -7,20 +7,24 @@
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
 
-
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
-
-
+raw_shootings_data <-
+  list_package_resources("f29feb49-ceb1-44bf-a2b6-5fc6a0e6147a") |>
+  filter(name == "Shooting Occurrences (2014-2019).csv") |>
+  get_resource()
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(
+  x = raw_shootings_data,
+  file = "inputs/data/raw_shootings_data.csv"
+  )
+
+
+
+
 
          
