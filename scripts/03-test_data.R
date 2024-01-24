866aacf012e0b90a65e8910398a2a27684469b58
Rahul Gopeesingh <rahulgopeesingh@Rahuls-MacBook-Air.local>
2024-01-23 19:46:18 -0500

    wrote tests for the cleaned data and ran them...all turned up "TRUE"

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,32 @@
 #### Preamble ####
 # Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Author: Rahul Gopeesingh
+# Date: 23 January 2024
+# Contact: rahul.gopeesingh@mail.utoronto.ca
+# Pre-requisites: executed the 01 and 02 script ie download and clean the data
+
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Test data ####
+
+#test that only the desired years show up in the occurred year column
+cleaned_shooting_data$OccurredYear |>
+  unique() == c(2014, 2015, 2016, 2017, 2018, 2019)
+
+#test that there are only 6 unique years in the occurred year column
+cleaned_shooting_data$OccurredYear |>
+  unique() |>
+  length == 6
+
+#test that 2014 was the first year in which data appears
+cleaned_shooting_data$OccurredYear |>
+  min() == 2014
+
+#test that every year has a number that is non-negative
+cleaned_shooting_data$Count_ |>
+  min() >= 0
+
