49d4871a9913319fba7e5f5d43218ca612636804
Rahul Gopeesingh <rahulgopeesingh@Rahuls-MacBook-Air.local>
2024-01-24 03:18:59 -0500

    Plotted graph based on given data. This commit includes both the script and the graph saved

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,37 +1,38 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Sorts the data from dataset in best form to read and then plots a graph based on said data
+# Author: Rahul Gopeesingh
+# Date: 24 January 2024
+# Contact: rahul.gopeesingh@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Completed the downloading and cleaning of data from previous scripts
 
 
 #### Workspace setup ####
 library(tidyverse)
-library(rstanarm)
+library(dplyr)
 
 #### Read data ####
-analysis_data <- read_csv("outputs/data/analysis_data.csv")
+cleaned_shooting_data <- read_csv("outputs/data/cleaned_shooting_data.csv")
 
 ### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+data_to_graph <- cleaned_shooting_data %>%
+  group_by(OccurredYear) %>%
+  summarise(Shootings = sum(Count_))
+#this sorts all the shootings in a year despite their locations and sums them together 
 
+write_csv(
+  x = data_to_graph,
+  file = "outputs/models/data_to_graph_table.csv"
+)
+#we plot our graph here and rename the axis to more readable formats. 
+data_to_graph |>
+  ggplot(aes(x = factor(OccurredYear), y = Shootings)) +
+  geom_col()+
+  theme_minimal()+
+  labs(x = "Year", y = "Number of Shootings in Toronto")
 
 #### Save model ####
-saveRDS(
-  first_model,
-  file = "outputs/models/first_model.rds"
-)
+ggsave(file = "outputs/models/graph.pdf")
+
 
 
