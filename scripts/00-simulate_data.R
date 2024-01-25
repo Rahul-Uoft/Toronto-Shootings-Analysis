756c296689b18c2f9fa2d35b93e5b4b2cf2d19ae
Rahul Gopeesingh <rahulgopeesingh@Rahuls-MacBook-Air.local>
2024-01-25 01:59:16 -0500

    Added meaningful comments to the simulate script

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -18,20 +18,23 @@ set.seed(42)
 #simulate the data as it would appear in the given dataset. This is because they are generally sorted by division
 simulated_data_shootings <- 
   tibble(
+    #first we simulate a column for the divisions and repeat them for each of the 6 years
     division = rep(c("D11","D12","D13","D14","D22","D23","D31","D32","D33","D41","D42","D43","D51","D52","D53","D54/55"), times = 6),
+    #next we simulate the column for the years in which each year must appear once for each division for a total of 16 times
     year = c(rep("2014", times = 16),
              rep("2015", times = 16),
              rep("2016", times = 16),
              rep("2017", times = 16),
              rep("2018", times = 16),
              rep("2019", times = 16)),
+    #finally we simulate the number of shooting using a unifrom distribution for each entry. This simulation is probably inaccurate but still results in data that should have the same qualities of the actual dataset with the exception of the values themselves
     number_of_shootings =
       runif(96, 0, 70)
   )
-simulated_data_shootings
 
 
 
+#we save the simulation locally so we can access the simulated data without having to run the simulation again.
 write_csv( 
   x = simulated_data_shootings,
   file = "inputs/data/simulated_dataset_shootings.csv"
